d4d755520306ef90e16a0e9fdcd60240ba0b81ef
Rishav parasar <rishavparasar2004@gmail.com>
2022-11-24 19:50:32 +0530

    uploading project starting on github

---
+++ src/conversions.java
@@ -0,0 +1,4 @@
+public class conversions
+{
+    //In this code, i am going to create different methods for corresponding different number system conversions
+}
---
+++ src/main_code.java
@@ -0,0 +1,4 @@
+public class main_code
+{
+    //I am going to make a program that performs conversion between different number system
+}
